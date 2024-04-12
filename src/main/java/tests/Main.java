e7fd1bf8f4bea78e0c31cb7aed5499531f5216c7
khalouiyesser <111162300+khalouiyesser@users.noreply.github.com>
2024-04-12 02:32:58 +0100

    création de ma branche

--- src/main/java/tests/Main.java
+++ src/main/java/tests/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         MyDatabase myDataBase = new MyDatabase();
+        System.out.println("hello");
         /* Test reclamation
         ServiceReclamation serviceReclamation = new ServiceReclamation();
 
