88afe03244b328f57d52a79322eac7e75c2df758
shaymaouerhani <shayma.ouerhani@esprit.tn>
2024-04-12 15:33:48 +0100

    interface client

--- src/main/java/controllers/ListeCompteAdmin.java
+++ src/main/java/controllers/ListeCompteAdmin.java
@@ -4,7 +4,7 @@ import java.net.URL;
 import javafx.fxml.Initializable;
 import java.util.ResourceBundle;
 
-public class ListeCompte implements Initializable {
+public class ListeCompteAdmin implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO)
