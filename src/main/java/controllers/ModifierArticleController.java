360b9e1d65b30ff5c3293083a264093e887765b7
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-04-13 20:15:03 +0100

    scrollAjouterArticle

---
+++ src/main/java/controllers/ModifierArticleController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javafx.scene.input.MouseEvent;
+
+public class ModifierArticleController {
+    public void open_CommandsList(MouseEvent mouseEvent) {
+    }
+
+    public void closeAddToCartModel(MouseEvent mouseEvent) {
+    }
+
+    public void close_addReviewsModel(MouseEvent mouseEvent) {
+    }
+}
