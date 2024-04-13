e2e73fad467f4b174be1eb7b6557440dbfeaed56
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-04-13 14:59:18 +0100

    listearticlesfrontSecondCommit

--- src/main/java/controllers/ArticleitemfrontController.java
+++ src/main/java/controllers/ArticleitemfrontController.java
@@ -1,4 +1,25 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
 public class ArticleitemfrontController {
+
+    @FXML
+    private HBox ArticleDetails;
+
+    @FXML
+    private Text contenuArtFront;
+
+    @FXML
+    private ImageView imgArtFront;
+
+    @FXML
+    private Text titreArtFront;
+
+    @FXML
+    private HBox voirPlusArtBtn;
+
 }
