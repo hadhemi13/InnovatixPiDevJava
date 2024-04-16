a0222424eb703a9f9f136e835203556649f0db60
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-04-16 13:10:26 +0100

    Interfacesmodifetsupp

--- src/main/java/controllers/ConfirmermodifController.java
+++ src/main/java/controllers/ConfirmermodifController.java
@@ -18,4 +18,55 @@ public class ConfirmermodifController {
 
     public void cancelAction(ActionEvent actionEvent) {
     }
+
+    public void setValue5(MouseEvent mouseEvent) {
+    }
+
+    public void hoverValue5(MouseEvent mouseEvent) {
+    }
+
+    public void setValue4(MouseEvent mouseEvent) {
+    }
+
+    public void hoverValue4(MouseEvent mouseEvent) {
+    }
+
+    public void hoverValue3(MouseEvent mouseEvent) {
+    }
+
+    public void setValue3(MouseEvent mouseEvent) {
+    }
+
+    public void setValue2(MouseEvent mouseEvent) {
+    }
+
+    public void setValue1(MouseEvent mouseEvent) {
+    }
+
+    public void hoverValue1(MouseEvent mouseEvent) {
+    }
+
+    public void hoverValue2(MouseEvent mouseEvent) {
+    }
+
+    public void update_review(MouseEvent mouseEvent) {
+    }
+
+    public void add_new_comment(MouseEvent mouseEvent) {
+    }
+
+    public void cancelHoverValue5(MouseEvent mouseEvent) {
+    }
+
+    public void cancelHoverValue4(MouseEvent mouseEvent) {
+    }
+
+    public void cancelHoverValue3(MouseEvent mouseEvent) {
+    }
+
+    public void cancelHoverValue1(MouseEvent mouseEvent) {
+    }
+
+    public void cancelHoverValue2(MouseEvent mouseEvent) {
+    }
 }
