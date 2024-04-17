45d8c7b8c51df89ab2208dd4636c4e87d83a284c
Mhmariem <mariem.mhamdi@esprit.tn>
2024-04-17 12:47:02 +0100

    Add user inworked.

--- src/main/java/controllers/SideNavBarController.java
+++ src/main/java/controllers/SideNavBarController.java
@@ -1,8 +1,6 @@
 package controllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -10,9 +8,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
-import java.io.IOException;
-
-public class DashbordAdminController {
+public class SideNavBarController {
 
     @FXML
     private HBox InvestBtn;
@@ -60,15 +56,6 @@ public class DashbordAdminController {
     private Label dashboardText;
 
     @FXML
-    private HBox evenementsBtn;
-
-    @FXML
-    private ImageView evenementsIcon;
-
-    @FXML
-    private Label evenementsText;
-
-    @FXML
     private ImageView investissementsIcon;
 
     @FXML
@@ -113,6 +100,7 @@ public class DashbordAdminController {
     @FXML
     private Label usersText;
 
+    private boolean sideBarVisible = true;
     @FXML
     void smallSide(MouseEvent event) {
 
@@ -120,36 +108,4 @@ public class DashbordAdminController {
 
 
 
-    @FXML
-    private void openUsersList(MouseEvent event) {
-        try {
-            // Chargement de l'interface utilisateur de la liste des utilisateurs
-            Parent fxml = FXMLLoader.load(getClass().getResource("src/main/resources/FXML/UsersList.fxml"));
-
-            // Remplacement du contenu existant par la nouvelle interface utilisateur
-            content_area.getChildren().setAll(fxml);
-
-            // Réinitialisation de l'état des boutons et des icônes
-
-            // Suppression de la classe de style "activeLink" de tous les boutons
-            usersBtn.getStyleClass().remove("activeLink");
-            dashboardBtn.getStyleClass().remove("activeLink");
-
-            // Suppression de la classe de style "activeText" de tous les textes associés aux boutons
-            usersText.getStyleClass().remove("activeText");
-            dashboardText.getStyleClass().remove("activeText");
-
-
-            // Définition de l'état actif pour le bouton "Users" et son texte associé
-            usersBtn.getStyleClass().add("activeLink");
-            usersText.getStyleClass().add("activeText");
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
 }
-
