2cbbb30a4d7da183b785c177f541229cfc3b0aa5
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-04-19 00:12:01 +0100

    delete et update from interface

--- src/main/java/controllers/ModifierArticleController.java
+++ src/main/java/controllers/ModifierArticleController.java
@@ -22,10 +22,12 @@ import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
 
 public class ModifierArticleController implements Initializable {
 
+    public Button ModifierArtBtn;
     @FXML
     private TextField titreArt;
 
@@ -47,6 +49,82 @@ public class ModifierArticleController implements Initializable {
     private Article article;
 
     private ServiceArticle serviceArticle;
+//
+//
+//    private void initializeArticleFields() {
+//        if (article != null) {
+//            titreArt.setText(article.getTitre_art());
+//            ContenuArt.setText(article.getContenu_art());
+//            datePubArt.setValue(article.getDate_pub_art().toLocalDate());
+//            addCategorie.setValue(article.getCategorie_art());
+//            SpinnerValueFactory<Integer> valueFactory = dureeArt.getValueFactory();
+//            if (valueFactory != null) {
+//                valueFactory.setValue(article.getDuree_art());
+//            }
+//            Image image = new Image(article.getImage_art());
+//            imageInput.setImage(image);
+//        }
+//    }
+
+
+//    private void loadArticleData(int articleId) {
+//        // Utilisez le service pour récupérer les détails de l'article à partir de la base de données
+//        article = this.article;
+//    }
+//
+//    // Mettre à jour la méthode initializeArticleFields() pour charger les données de l'article
+//    private void initializeArticleFields(int articleId) {
+//        loadArticleData(articleId); // Charger les données de l'article depuis la base de données
+//        // Reste du code inchangé
+//    }
+//
+//    // Mettre à jour la méthode ModifierArt(MouseEvent mouseEvent) pour effectuer la mise à jour de l'article
+//    @FXML
+//    public void ModifierArt(MouseEvent mouseEvent) {
+//        try {
+//            // Mettre à jour les valeurs de l'article avec les nouvelles valeurs saisies par l'utilisateur
+//            article.setTitre_art(titreArt.getText());
+//            article.setContenu_art(ContenuArt.getText());
+//            article.setDate_pub_art(LocalDateTime.of(datePubArt.getValue(), LocalTime.MIDNIGHT));
+//            article.setCategorie_art(addCategorie.getValue());
+//            article.setDuree_art(dureeArt.getValue());
+//            article.setImage_art(imageInput.getImage().getUrl());
+//
+//            // Appeler la méthode de service pour effectuer la mise à jour de l'article dans la base de données
+//            serviceArticle.modifier(article);
+//
+//            // Fermer la fenêtre après la mise à jour
+//            Stage stage = (Stage) titreArt.getScene().getWindow();
+//            stage.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            // Gérer l'exception appropriée ici
+//        }
+//    }
+//public void initData(Article article) {
+//    this.article = article;
+//    initializeArticleFields();
+//}
+//
+//    private void initializeArticleFields() {
+//        if (article != null) {
+//            titreArt.setText(article.getTitre_art());
+//            ContenuArt.setText(article.getContenu_art());
+//            datePubArt.setValue(article.getDate_pub_art().toLocalDate());
+//            addCategorie.setValue(article.getCategorie_art());
+//            SpinnerValueFactory<Integer> valueFactory = dureeArt.getValueFactory();
+//            if (valueFactory != null) {
+//                valueFactory.setValue(article.getDuree_art());
+//            }
+//            Image image = new Image(article.getImage_art());
+//            imageInput.setImage(image);
+//        }
+//    }
+
+    public void initData(Article article) {
+        this.article = article;
+        initializeArticleFields();
+    }
 
     private void initializeArticleFields() {
         if (article != null) {
@@ -58,11 +136,40 @@ public class ModifierArticleController implements Initializable {
             if (valueFactory != null) {
                 valueFactory.setValue(article.getDuree_art());
             }
-            Image image = new Image(article.getImage_art());
-            imageInput.setImage(image);
+            // Assurez-vous que l'image de l'article n'est pas vide
+            if (article.getImage_art() != null && !article.getImage_art().isEmpty()) {
+                Image image = new Image(article.getImage_art());
+                imageInput.setImage(image);
+            }
         }
     }
 
+    @FXML
+    void ModifierArt(MouseEvent mouseEvent) {
+        try {
+            if (article != null) {
+                article.setTitre_art(titreArt.getText());
+                article.setContenu_art(ContenuArt.getText());
+                article.setDate_pub_art(LocalDateTime.of(datePubArt.getValue(), LocalTime.MIDNIGHT));
+                article.setCategorie_art(addCategorie.getValue());
+                article.setDuree_art(dureeArt.getValue());
+                // Assurez-vous que l'image de l'article n'est pas vide
+                if (imageInput.getImage() != null) {
+                    article.setImage_art(imageInput.getImage().getUrl());
+                }
+                // Appeler la méthode de service pour effectuer la mise à jour de l'article dans la base de données
+                ServiceArticle serviceArticle = new ServiceArticle();
+                serviceArticle.modifier(article);
+
+                // Fermer la fenêtre après la mise à jour
+                Stage stage = (Stage) titreArt.getScene().getWindow();
+                stage.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Gérer l'exception appropriée ici
+        }
+    }
     @FXML
     void ajouter_image(MouseEvent mouseEvent) {
         FileChooser fileChooser = new FileChooser();
@@ -113,19 +220,20 @@ public class ModifierArticleController implements Initializable {
         }));
 
         // Initialiser les champs de l'article
-        initializeArticleFields();
+       // initializeArticleFields();
     }
+//
+//    public void ModifierArt(MouseEvent mouseEvent) throws SQLException {
+//        ServiceArticle serviceArticle = new ServiceArticle() ;
+//        if (article != null) {
+//            article.setTitre_art(titreArt.getText());
+//            article.setContenu_art(ContenuArt.getText());
+//            article.setDate_pub_art(LocalDateTime.of(datePubArt.getValue(), LocalDate.now().atStartOfDay().toLocalTime()));
+//            article.setCategorie_art(addCategorie.getValue());
+//            article.setDuree_art(dureeArt.getValue());
+//            article.setImage_art(imageInput.getImage().getUrl());
+//            serviceArticle.modifier(article);
+//        }
+//    }
 
-    public void ModifierArt(MouseEvent mouseEvent) throws SQLException {
-        ServiceArticle serviceArticle = new ServiceArticle() ;
-        if (article != null) {
-            article.setTitre_art(titreArt.getText());
-            article.setContenu_art(ContenuArt.getText());
-            article.setDate_pub_art(LocalDateTime.of(datePubArt.getValue(), LocalDate.now().atStartOfDay().toLocalTime()));
-            article.setCategorie_art(addCategorie.getValue());
-            article.setDuree_art(dureeArt.getValue());
-            article.setImage_art(imageInput.getImage().getUrl());
-            serviceArticle.modifier(article);
-        }
-    }
 }
