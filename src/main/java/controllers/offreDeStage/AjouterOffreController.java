592de164b7cd92f5630ed1b9a52056295cd01e4b
khalouiyesser <111162300+khalouiyesser@users.noreply.github.com>
2024-04-23 09:42:44 +0100

    ajouter offre avec controle de saisie.

--- src/main/java/controllers/offreDeStage/AjouterOffreController.java
+++ src/main/java/controllers/offreDeStage/AjouterOffreController.java
@@ -1,31 +1,71 @@
 package controllers.offreDeStage;
 
 import Entities.OffreDeStage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import services.ServiceOffreDeStage;
+import services.Upload;
+import services.ValidatorOffre;
 
+import java.io.File;
+import java.net.URL;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
-public class AjouterOffreController {
-
-    @FXML
-    private DatePicker date;
-
-    @FXML
-    private TextField titreInput;
+public class AjouterOffreController implements Initializable {
 
     @FXML
     private TextField motsCles;
 
     @FXML
-    private TextField typeOffre;
+    private Text TitreError;
+
+    @FXML
+    private Text LanguageError;
+
+    @FXML
+    private Text experienceError;
+
+    @FXML
+    private Text DomaineError;
+
+    @FXML
+    private Text PosteError;
+
+    @FXML
+    private Text TypeError;
+
+    @FXML
+    private Label ExigenceError;
+
+    @FXML
+    private Text NiveauError;
+
+    @FXML
+    private Text DescriptionError;
 
     @FXML
     private Button upload;
+
+    @FXML
+    private ComboBox<String> type;
+
+    @FXML
+    private TextField titre;
+
+    @FXML
+    private TextField domaine;
 
     @FXML
     private TextField description;
@@ -34,43 +74,142 @@ public class AjouterOffreController {
     private TextField language;
 
     @FXML
-    private TextField experience;
+    private ComboBox<String> experience;
+
 
     @FXML
     private TextField niveau;
+    @FXML
+    private ImageView pfeImg;
 
     @FXML
     private TextField pfeBook;
 
     @FXML
-    private TextField domaineInput;
+    private Button updateOffre;
 
     @FXML
-    private TextField Exigence;
+    private TextField exigence;
 
     @FXML
-    private TextField postePropose;
+    private TextField poste;
 
     @FXML
-    private AnchorPane content_area;
+    private Label MotsClesError;
+    File selectedCvFile;
+    String fileName;
 
-    @FXML
-    private Button ajouterOffre;
+
     @FXML
     void ajouterOffre(ActionEvent event) throws SQLException {
+        SingleSelectionModel<String> experienceSelectionModel = experience.getSelectionModel();
+        String selectedexperience = experienceSelectionModel.getSelectedItem();
+
+        SingleSelectionModel<String> experienceSelectionModel1 = type.getSelectionModel();
+        String selectedtype = experienceSelectionModel1.getSelectedItem();
+
         ServiceOffreDeStage serviceOffreDeStage = new ServiceOffreDeStage();
-        OffreDeStage stage = new OffreDeStage(titreInput.getText(),domaineInput.getText(),typeOffre.getText(),Integer.parseInt(postePropose.getText()),experience.getText(),description.getText(),Exigence.getText());
-        serviceOffreDeStage.ajouter(stage);
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Information manquante");
-        alert.setHeaderText(null);
-        alert.setContentText("Toutes les champs sont vides.");
-        Optional<ButtonType> option = alert.showAndWait();
+
+        // Vérifie si le champ "poste" n'est pas vide
+//        String posteText = poste.getText();
+//
+//        // Convertit la valeur du champ "poste" en entier
+//        int posteValue = Integer.parseInt(posteText);
+
+        // Obtient la date d'aujourd'hui
+        LocalDate today = LocalDate.now();
+        // Controle de saisie
+        ValidatorOffre validatorOffre = new ValidatorOffre();
+
+
+        if (!validatorOffre.isValidTitreOffre(titre.getText())){
+            TitreError.setVisible(true);
+            TitreError.setText("Le titre doit contient plus que 20 caractère !");
+        }
+        if (!validatorOffre.isValidEmpty(domaine.getText())){
+            DomaineError.setVisible(true);
+        }
+        if (!validatorOffre.isValidEmpty(selectedtype)){
+            TypeError.setVisible(true);
+        }
+        if (!validatorOffre.isValidEmpty(selectedexperience)){
+            ExigenceError.setVisible(true);
+        }
+        if (!validatorOffre.isValidEmpty(niveau.getText())){
+            NiveauError.setVisible(true);
+        }
+        if (!validatorOffre.isValidEmpty(language.getText())){
+            LanguageError.setVisible(true);
+        }
+        if (!validatorOffre.isValidDescriptionOffre(description.getText())){
+            DescriptionError.setVisible(true);
+            DescriptionError.setText("La description doit conetient plus que 100 caractère !");
+        }
+        if (!validatorOffre.isValidDescriptionOffre(exigence.getText())){
+            ExigenceError.setVisible(true);
+            ExigenceError.setText("L'éxigence  doit conetient plus que 100 caractère !");
+        }
+        if (    validatorOffre.isValidDescriptionOffre(exigence.getText()) && validatorOffre.isValidDescriptionOffre(description.getText())
+                && validatorOffre.isValidEmpty(language.getText()) && validatorOffre.isValidEmpty(niveau.getText())&&
+                validatorOffre.isValidEmpty(selectedexperience) && validatorOffre.isValidEmpty(selectedtype)
+                && validatorOffre.isValidEmpty(domaine.getText()) && validatorOffre.isValidTitreOffre(titre.getText()) )
+        {
+            OffreDeStage stage = new OffreDeStage(Integer.parseInt(poste.getText()),titre.getText(),domaine.getText(),selectedtype,selectedexperience,description.getText(),exigence.getText(),Date.valueOf(today));
+            serviceOffreDeStage.ajouter(stage);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Modification effectuée");
+            alert.setHeaderText(null);
+            alert.setContentText("L'offre de stage a été modifiée avec succès.");
+            alert.showAndWait();
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Echec");
+            alert.setHeaderText(null);
+            alert.setContentText("Les champs ne sont pas validés.");
+            alert.showAndWait();
+        }
+
     }
+
 
     @FXML
     void upload(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choisir votre CV");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
+        selectedCvFile = fileChooser.showOpenDialog(upload.getScene().getWindow());
+
+        if (selectedCvFile != null) {
+            Upload serviceUpload = new Upload();
+            fileName = serviceUpload.Upload(selectedCvFile);
+        }
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> beneficiaires = FXCollections.observableArrayList(
+                "Offre d'emploi",
+                "Offre de stage"
+        );
+        ObservableList<String> beneficiaire = FXCollections.observableArrayList(
+                "0-2 ans",
+                "2-4 ans",
+                "5 ans +"
+        );
+
+        experience.setItems(beneficiaire);
+        type.setItems(beneficiaires);
+        TitreError.setVisible(false);
+        LanguageError.setVisible(false);
+        experienceError.setVisible(false);
+        DomaineError.setVisible(false);
+        PosteError.setVisible(false);
+        TypeError.setVisible(false);
+        ExigenceError.setVisible(false);
+        NiveauError.setVisible(false);
+        DescriptionError.setVisible(false);
+        MotsClesError.setVisible(false);
+    }
 }
