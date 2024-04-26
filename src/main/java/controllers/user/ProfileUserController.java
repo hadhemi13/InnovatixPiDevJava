585b198f0693ea2f79d01a41ada45ec36d568ea7
Mhmariem <mariem.mhamdi@esprit.tn>
2024-04-26 03:53:02 +0100

    change password

--- src/main/java/controllers/user/ProfileUserController.java
+++ src/main/java/controllers/user/ProfileUserController.java
@@ -1,13 +1,27 @@
 package controllers.user;
 
+import Entities.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import services.ServiceUser;
 
-public class ProfileUserController {
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ProfileUserController  implements Initializable {
 
     @FXML
     private Button changePassBTN;
@@ -35,6 +49,50 @@ public class ProfileUserController {
 
     @FXML
     private ImageView userItemUpdateBtnImg;
+    @Override
 
+    public void initialize(URL location, ResourceBundle resources) {
+    ServiceUser userService = new ServiceUser();
+        try {
+        // user = userService.getOneUser(UserSession.getInstance().getEmail());
+            User user;
+            if (UserSession.getInstance().getEmail() == null) {
+            user = userService.getOneUser("mariem@gmail.com");
+        } else {
+            user = userService.getOneUser(UserSession.getInstance().getEmail());
+        }
+
+       // Image image = new Image(
+               // getClass().getResource("/img/admin.png/" + user.getPhoto()).toExternalForm());
+       // userItemImg.setImage(image);
+
+
+
+
+
+        fullnameText.setText(user.getName());
+        descriptionText.setText(user.getRoles());
+        emailText.setText(user.getEmail());
+        telText.setText(user.getTel());
+
+
+    } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    void changePass(ActionEvent event) throws IOException {
+        try {
+            // Charger le fichier FXML de listArticleAdmin
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/ChangePassword.fxml"));
+            Pane listArticleAdminPane = loader.load();
+
+            // Remplacer le contenu de content_area par le contenu
+            profilePane.getChildren().setAll(listArticleAdminPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
 
