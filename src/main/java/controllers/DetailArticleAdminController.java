f1b536435ffb445290c0ddc0895c3f1018308a55
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-05-02 06:55:27 +0100

    article commentaire detail admin avec upload

--- src/main/java/controllers/DetailArticleAdminController.java
+++ src/main/java/controllers/DetailArticleAdminController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -16,8 +17,12 @@ import javafx.scene.text.Text;
 import services.ServiceArticle;
 import services.ServiceCommentaireHadhemi;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -93,13 +98,14 @@ public class DetailArticleAdminController implements Initializable {
 
     @FXML
     private Text titreArt;
-
+    @FXML
+    private Text piece;
     @FXML
     private VBox CommentListContainer;
     @FXML
     private Text total_verif_reviews;
     Article article;
-    public int idAuto ;
+    public int idAuto;
 
     public void setData(Article article) {
         this.article = article;
@@ -122,42 +128,58 @@ public class DetailArticleAdminController implements Initializable {
             description.setText(article.getContenu_art());
             idAuto = article.getId();
             System.out.println("setArticle is called avec id  " + idAuto);
+            piece.setOnMouseClicked(this::openArticleImage);
 
         }
 
 
-        //////////////////////Ajb/////////////////
+            //////////////////////Ajb/////////////////
 
-        ServiceCommentaireHadhemi sch = new ServiceCommentaireHadhemi();
-        List<CommentaireHadhemi> list = new ArrayList<>();
-        try {
-            System.out.println("000111 id of this article is :: " + idAuto);
-
-            //list = sch.afficher();
-
-            System.out.println("000222 id of this article is :: " + idAuto);
-            list = sch.afficherById(idAuto);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        for (CommentaireHadhemi commentaireHadhemi : list) {
+            ServiceCommentaireHadhemi sch = new ServiceCommentaireHadhemi();
+            List<CommentaireHadhemi> list = new ArrayList<>();
             try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/commentaireItemFront.fxml"));
-                Parent offreItem = loader.load();
-                CommentaireItemFrontController ComItem = loader.getController();
-                System.out.println("contenue du comm page1 " + ComItem);
-                ComItem.initData(commentaireHadhemi);
-                CommentListContainer.getChildren().add(offreItem);
-            } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("000111 id of this article is :: " + idAuto);
+
+                //list = sch.afficher();
+
+                System.out.println("000222 id of this article is :: " + idAuto);
+                list = sch.afficherById(idAuto);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
+            for (CommentaireHadhemi commentaireHadhemi : list) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/commentaireItemFront.fxml"));
+                    Parent offreItem = loader.load();
+                    CommentaireItemFrontController ComItem = loader.getController();
+                    System.out.println("contenue du comm page1 " + ComItem);
+                    ComItem.initData(commentaireHadhemi);
+                    CommentListContainer.getChildren().add(offreItem);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
 
+        @Override
+        public void initialize (URL url, ResourceBundle resourceBundle){
+            article = this.article;
+        }
+
+    private void openArticleImage(MouseEvent mouseEvent) {
+        Path destination = Paths.get(System.getProperty("user.dir"), "src", "main", "java", "uploadsPdfH", article.getPiecejointe_art());
+
+        try {
+            File file = destination.toFile();
+            if (file.exists()) {
+                Desktop.getDesktop().open(file);
+            } else {
+                System.out.println("File not found: " + destination);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        article=this.article;
-    }
-}
+}}
