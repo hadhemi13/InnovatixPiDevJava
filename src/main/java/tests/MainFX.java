a9628f8befafba6441a89b573d8b40419ef627fb
hadhemi133 <hadhemi.mahmoud@esprit.tn>
2024-05-07 21:58:30 +0100

    articles

--- src/main/java/tests/MainFX.java
+++ src/main/java/tests/MainFX.java
@@ -11,8 +11,8 @@ public class MainFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 //        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/SideNavBar.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/SideNavBar.fxml"));
-
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/article/ListArticleFront.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/front.fxml"));
 //        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/listeArticlesClients.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
